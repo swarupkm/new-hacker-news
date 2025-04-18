3a01d02d4ce4077a6e70649da1d0f2dcab3ee900
Swarup Mahapatra <swarup@Swarups-MacBook-Pro.local>
2025-04-18 16:50:58 +0530

    Add pagination

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,14 +2,17 @@ import { useEffect, useState } from 'react';
 import { fetchTopStories, fetchStories } from './services/api';
 import { Story } from './Story';
 
+const PAGE_SIZE = 30
+
 function Home() {
   const [stories, setStories] = useState([]);
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
+  const [pageNumber, setPageNumber] = useState(1);
 
   useEffect(() => {
     async function load() {
       try {
-        const storyIds = await fetchTopStories(30);
+        const storyIds = await fetchTopStories(pageNumber, PAGE_SIZE);
         const fetchedStories = await fetchStories(storyIds);
         setStories(fetchedStories);
       } catch (error) {
@@ -19,10 +22,34 @@ function Home() {
       }
     }
     load();
-  }, [])
+    return () => {
+      setLoading(true)
+    }
+  }, [pageNumber])
+  function goToPrevious() {
+    if (pageNumber === 1) return;
+    setPageNumber(pageNumber - 1);
+  }
+
+  function goToNext() {
+    if (stories.length < PAGE_SIZE) return
+    setPageNumber(pageNumber + 1);
+  }
+
   return (<>
+    <div>  
     {loading ? <div> Loading ... </div> :
-      stories.map((story, index) => (<Story key={index} story={story} idx={index + 1} />))}
+      stories.map((story, index) => (<Story key={index} story={story} idx={(pageNumber-1)*30 + index + 1} />))}
+    </div>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '20px', gap: '10px' }}>
+      <button style={{ padding: '10px 20px', cursor: 'pointer', borderRadius: '5px', border: '1px solid #ccc', backgroundColor: '#f0f0f0' }} onClick={goToPrevious}>
+      Prev
+      </button>
+      <span style={{ fontWeight: 'bold' }}>Current: {pageNumber}</span>
+      <button style={{ padding: '10px 20px', cursor: 'pointer', borderRadius: '5px', border: '1px solid #ccc', backgroundColor: '#f0f0f0' }} onClick={goToNext}>
+      Next
+      </button>
+    </div>
   </>)
 }
 export default Home;
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,17 +1,19 @@
 const BASE_URL = 'https://hacker-news.firebaseio.com/v0';
 
-export const fetchTopStories = async (limit = 30) => {
+export const fetchTopStories = async (pageNumber: number, pageSize = 30) => {
   try {
     const response = await fetch(`${BASE_URL}/topstories.json`);
     const storyIds = await response.json();
-    return storyIds.slice(0, limit);
+    const start = (pageNumber - 1) * pageSize
+    const end = start + pageSize
+    return storyIds.slice(start, end);
   } catch (error) {
     console.error('Error fetching top stories:', error);
     throw error;
   }
 };
 
-export const fetchItem = async (id) => {
+export const fetchItem = async (id: string) => {
   try {
     const response = await fetch(`${BASE_URL}/item/${id}.json`);
     return await response.json();
@@ -21,7 +23,7 @@ export const fetchItem = async (id) => {
   }
 };
 
-export const fetchStories = async (storyIds) => {
+export const fetchStories = async (storyIds: string[]) => {
   try {
     const stories = await Promise.all(
       storyIds.map(id => fetchItem(id))
