3d1e11f54bf82bc5be839b66e365e939ee90ba7c
Swarup Mahapatra <swarup@Swarups-MacBook-Pro.local>
2025-04-19 21:45:27 +0530

    Add favicon and update title of browser

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,11 +3,15 @@ import { Link } from 'react-router';
 function Header() {
   return (
     <header className="bg-[#ff6600] py-1.5">
-      <nav className="max-w-4xl mx-auto px-2">
-        <Link to="/" className="inline-block font-bold text-[13.3px] text-black hover:text-gray-800">
+      <nav className="max-w-4xl mx-auto px-2 flex items-center space-x-2">
+        <Link to="/">
+          <img src='src/assets/logo.svg' className='border'/>
+        </Link>
+        <Link to="/" className="font-bold text-[13.3px] text-black hover:text-gray-800">
           Hacker News
-        </Link> |
-        <Link to="/newest">
+        </Link>
+        <span className="text-black">|</span>
+        <Link to="/newest" className="text-[13.3px] text-black hover:text-gray-800">
           new
         </Link>
       </nav>
